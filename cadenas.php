13b270a5bdf33969a3376f3ab9845e0146cf2248
Xope <errchosep25@gmail.com>
2022-09-16 13:13:38 +0200

    falten els de url

--- cadenas.php
+++ cadenas.php
@@ -1,4 +1,20 @@
 <?php
     $nombre = $_GET['nombre'] ?? 'Josep';
-    echo "$nombre";
+    $nombre = trim($nombre,"/");
+    echo $nombre . "<br/>";
+    echo strlen($nombre) . "<br/>";
+    echo strtoupper($nombre) . "<br/>";
+    echo strtolower($nombre) . "<br/>";
+    $prefijo = $_GET['prefijo'] ?? null;
+    if($prefijo != null && strpos($nombre,$prefijo) === 0){
+        echo "El nombre empieza por $prefijo <br/>";
+    }
+    echo substr_count(strtolower($nombre),"a") . "<br/>";
+    $primeraA = stripos($nombre, "a");
+    if($primeraA === false) {
+        echo "No hay ninguna 'a' <br/>";
+    } else {
+        echo $primeraA . "<br/>";
+    }
+    echo str_ireplace("o", "0", $nombre);
 ?>

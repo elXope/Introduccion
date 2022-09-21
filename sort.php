7ff830f5c08d0746b8c3a4cc3df164c1fb42d51b
Xope <errchosep25@gmail.com>
2022-09-21 08:23:36 +0200

    Acabat

--- sort.php
+++ sort.php
@@ -11,7 +11,16 @@
         $edades=array("Juan"=>"31","María"=>"41","Andrés"=>"39","Berta"=>"40");
         print_r($edades);
         echo "<br/>";
+        ksort($edades);
+        print_r($edades);
+        echo "<br/>";
         asort($edades);
+        print_r($edades);
+        echo "<br/>";
+        krsort($edades);
+        print_r($edades);
+        echo "<br/>";
+        arsort($edades);
         print_r($edades);
     ?>
 </body>

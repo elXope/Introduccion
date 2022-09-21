ec6043b65b9cf961081554e1f030c385ee0c9142
Xope <errchosep25@gmail.com>
2022-09-21 10:39:26 +0200

    Gens acabat

---
+++ partlist.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Partlist</title>
+</head>
+<body>
+    <?php
+        $arreglo = explode(" ", "Seguro que apruebo esta asignatura");
+        $solucion = [];
+        for($i = 1; $i < count($arreglo); $i++) {
+            $solucion[$i - 1] = [implode(array_slice($arreglo, 0, $i)), implode(array_slice($arreglo, $i, ))]
+        }
+    ?>
+</body>
+</html>

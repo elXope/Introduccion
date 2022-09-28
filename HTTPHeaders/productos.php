e0ae0134ff00a61c0c2e79e20201ae5f003627a3
Xope <errchosep25@gmail.com>
2022-09-28 10:21:10 +0200

    Fet sense el error 404

---
+++ HTTPHeaders/productos.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<?php
+$productos = ["1" => "Producto 1", "2" => "Producto 2", "3" => "Producto 3"];
+$id = $_GET["id"] ?? -1;
+if (array_search($id, array_keys($productos)) === false) {
+    exit("No existe el producto");
+}
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Productos</title>
+</head>
+<body>
+    <h1>Productos</h1>
+    <p>Ejercicio 1 de cabeceras de respuesta.<br/>Producto solicitado: <?=$productos[$id]?></p>
+</body>
+</html>

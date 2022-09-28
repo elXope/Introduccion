5399c2c19cf40e2103f836b0f7434d2cb1ea334e
Xope <errchosep25@gmail.com>
2022-09-28 10:24:41 +0200

    Acaabt en la funció array_key_exists

--- HTTPHeaders/productos.php
+++ HTTPHeaders/productos.php
@@ -2,7 +2,7 @@
 <?php
 $productos = ["1" => "Producto 1", "2" => "Producto 2", "3" => "Producto 3"];
 $id = $_GET["id"] ?? -1;
-if (array_search($id, array_keys($productos)) === false) {
+if (!array_key_exists($id, $productos)) {
     exit("No existe el producto");
 }
 ?>

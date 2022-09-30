3083bd2d700b6403ba9503b1a0e057c4d7d3379b
Xope <errchosep25@gmail.com>
2022-09-30 11:43:34 +0200

    Acabat afegint exit()

---
+++ HTTPHeaders/privada.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<?php
+// function ixir() {
+//     header('Location:login.php');
+//     exit();
+// }
+// $_GET["dejameEntrar"] == 1 ?: ixir();
+if ($_GET["dejameEntrar"] == 1) {
+    header('Location:login.php');
+    exit();
+}
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Privada</title>
+</head>
+<body>
+    <h1>Bienvenido</h1>
+</body>
+</html>

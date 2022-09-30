c86182953aaddfad4037658bc20ccf0bcd6c18b5
Xope <errchosep25@gmail.com>
2022-09-30 11:44:14 +0200

    Acabat usant exit()

--- HTTPHeaders/login.php
+++ HTTPHeaders/login.php
@@ -1,19 +1,12 @@
 <!DOCTYPE html>
-<?php
-// $_GET["dejameEntrar"] == 1 ?: header('Location:login.php');
-if ($_GET["dejameEntrar"] == 1) {
-    header('Location:login.php');
-    exit();
-}
-?>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Privada</title>
+    <title>Login</title>
 </head>
 <body>
-    <h1>Bienvenido</h1>
+    <h1>Debes hacer login primero</h1>
 </body>
 </html>

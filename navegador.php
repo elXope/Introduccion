b78db3307f9c9e14df653f32536bbb5590361a1d
Xope <errchosep25@gmail.com>
2022-09-30 11:27:56 +0200

    Acabat

---
+++ navegador.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<script language="javascript">alert("Tiene que abrirse con Firefox")</script><html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Navegador</title>
+</head>
+<body>
+    <h1>Navegador</h1>
+    <p>Esta página sólo se mostrará en navegador Firefox.</p>
+</body>
+</html>
